980cc85d8253e06e5fc7c37a4b35a85e941f8dbc
don-harris <donald.walter.harris@gmail.com>
2017-08-15 10:43:03 +1200

    Update eloquent-2.js

--- eloquent-2.js
+++ eloquent-2.js
@@ -1,1 +1,30 @@
+//looping triangle
+for (var line = "#"; line.length < 8; line += "#")
+  console.log(line);
 
+//fizzbuzz!!
+for (var n = 1; n <= 100; n++) {
+  var output = "";
+  if (n % 3 == 0)
+    output += "Fizz";
+  if (n % 5 == 0)
+    output += "Buzz";
+  console.log(output || n);
+}
+
+//Chess
+var size = 8;
+
+var board = "";
+
+for (var y = 0; y < size; y++) {
+  for (var x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0)
+      board += " ";
+    else
+      board += "#";
+  }
+  board += "\n";
+}
+
+console.log(board);
